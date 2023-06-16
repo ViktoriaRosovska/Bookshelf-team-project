c4315596c0be98493c01e4823ac35a7a5544bd93
DmytroKrylov <120196125+DmytroKrylov@users.noreply.github.com>
2023-06-16 20:55:56 +0300

    support_new_upd

--- src/js/sponsors-obj.js
+++ src/js/sponsors-obj.js
@@ -3,46 +3,46 @@ export const sponsors =[
     title: 'Save the Children',
     url:
       'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: 'save_the_children.png',
+    img: '/save_the_children.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: 'project_hope.png',
+    img: '/progecthope.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: 'united24.png',
+    img: '',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: 'imc.png',
+    img: '',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: 'medecins_sans.png',
+    img: '',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: 'razom.png',
+    img: '',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: 'action_against_hunger.png',
+    img: '',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: 'world_vision.png',
+    img: '',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: 'sergiy_prytula.png',
+    img: '',
   },
 ]
