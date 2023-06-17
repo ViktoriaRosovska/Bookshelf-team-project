1ee48c69de831436d650e63838af7d5dca2fa8ad
Viktoria Rosovska <torimeister@gmail.com>
2023-06-17 18:54:57 +0300

    add modal pop-up

--- src/js/sponsors-obj.js
+++ src/js/sponsors-obj.js
@@ -3,46 +3,46 @@ export const sponsors =[
     title: 'Save the Children',
     url:
       'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: 'save_the_children.png',
+    img: 'save_the_children',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: 'progecthope.png',
+    img: 'progecthope',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: 'united24.png',
+    img: 'united24',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: 'imc.png',
+    img: 'imc',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: 'medecins_sans.png',
+    img: 'medecins_sans',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: 'razom.png',
+    img: 'razom',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: 'action_against.png',
+    img: 'action_against',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: 'world_vision.png',
+    img: 'world_vision',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: 'sergiy_prytula.png',
+    img: 'sergiy_prytula',
   },
 ]
