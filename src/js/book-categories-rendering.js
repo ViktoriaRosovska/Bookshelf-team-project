3da4965cacb821ea01904f8d1e7a3df2a8a3e099
Viktoria Rosovska <torimeister@gmail.com>
2023-06-18 09:44:27 +0300

    change book rendering

--- src/js/book-categories-rendering.js
+++ src/js/book-categories-rendering.js
@@ -1,49 +1,59 @@
-const BASE_URL_MY =
-  'https://books-backend.p.goit.global/books/category?category=';
-const ENDPOINT = 'Series%20Books';
-const API_KEY = '';
-let page = 1;
-const list = document.querySelector('.js-list-rendering');
-const loadMore = document.querySelector('.js-load_more-rendering');
-loadMore.addEventListener('click', handlerPagination);
+import { APIService } from "./API-service";
 
-function handlerPagination() {
-  page += 1;
-  servisMovi(page)
-    .then(data => {
-      list.insertAdjacentHTML('beforeend', createMarkup(data.results));
-    })
-    .catch(err => console.log(err));
-}
+// const BASE_URL_MY =
+//   'https://books-backend.p.goit.global/books/category?category=';
+// const ENDPOINT = 'Series%20Books';
+// const API_KEY = '';
+// let page = 1;
+// const list = document.querySelector('.js-list-rendering');
+// const loadMore = document.querySelector('.js-load_more-rendering');
 
-function servisBook(page) {
-  return fetch(`${BASE_URL_MY}${ENDPOINT}`).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
-}
+const bookCollection = document.querySelector('.books-gallery');
+const api1 = new APIService();
 
-servisBook()
-  .then(data => {
-    list.insertAdjacentHTML('beforeend', createMarkup(data));
-  })
-  .catch(err => console.log(err));
 
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({
-        title,
+export default  async function createMarkup(category){
+    const res = await api1.fetchBooksByCategory(category);
+    const books = await res.data;
+    console.log(books);
+    const collectionMarkup = books.map(({title,
         book_image,
         author,
-        _id,
-      }) => `<li class="flex-element" id="${_id}">
+        _id,}) => {
+       return  `<li class="flex-element" id="${_id}">
       <img src="${book_image}" alt="${title}">
       <h2>${title}</h2>
       <p>${author}</p>
   </li>`
-    )
+    })
     .join('');
-}
+    bookCollection.innerHTML = collectionMarkup;
+  }
+
+
+
+// loadMore.addEventListener('click', handlerPagination);
+
+
+// servisBook()
+//   .then(data => {
+//     list.insertAdjacentHTML('beforeend', createMarkup(data));
+//   })
+//   .catch(err => console.log(err));
+
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({
+//         title,
+//         book_image,
+//         author,
+//         _id,
+//       }) => `<li class="flex-element" id="${_id}">
+//       <img src="${book_image}" alt="${title}">
+//       <h2>${title}</h2>
+//       <p>${author}</p>
+//   </li>`
+//     )
+//     .join('');
+// }
