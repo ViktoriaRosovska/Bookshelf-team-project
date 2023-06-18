92893c1d486ec3495fcd7d3eb609506b395bf88d
OksanaPonom <ksyushono4ek@gmail.com>
2023-06-18 13:53:31 +0300

    add markup card

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { sponsors } from './js/sponsors-obj';
+import './js/shopping-list';
 import './js/sponsors-list';
 import './js/book-categories';
 import './js/scrollBtn';
@@ -9,5 +10,3 @@ import './js/book-categories-rendering';
 import './js/modal-book-card';
 
 import './js/modal-mobile-menu';
-
-
