53146988ec7ca12a2bade4c86b3a639a66b26316
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-06-19 15:42:43 +0200

    +++

--- src/js/modal-mobile-menu.js
+++ src/js/modal-mobile-menu.js
@@ -9,24 +9,24 @@ const body = document.body;
 const homeBtnEl = document.querySelector('.js-modal-home-btn')
 const listBtnEl = document.querySelector('.js-modal-list-btn')
 
-burgerBtnEl.addEventListener('click', showModal)
-closeModalBtnEl.addEventListener('click', closeModal)
+burgerBtnEl?.addEventListener('click', showModal)
+closeModalBtnEl?.addEventListener('click', closeModal)
 
 function showModal(elem) {
-    headerEl.classList.add('index')
-    wraperMobileMenuEl.classList.add('no-scroll')
-    body.classList.add('no-scroll')
-    wraperMobileMenuEl.classList.remove('is-hidden-mobile')
+    headerEl?.classList.add('index')
+    wraperMobileMenuEl?.classList.add('no-scroll')
+    body?.classList.add('no-scroll')
+    wraperMobileMenuEl?.classList.remove('is-hidden-mobile')
     // menuMobileEl.classList.add('active')
-    burgerBtnEl.classList.add('is-hidden-btn')
-    closeModalBtnEl.classList.remove('is-hidden-btn')
+    burgerBtnEl?.classList.add('is-hidden-btn')
+    closeModalBtnEl?.classList.remove('is-hidden-btn')
 
 }
 
 function closeModal(elem) {
-    wraperMobileMenuEl.classList.remove('no-scroll')
-    wraperMobileMenuEl.classList.add('is-hidden-mobile')
-    burgerBtnEl.classList.remove('is-hidden-btn')
-    closeModalBtnEl.classList.add('is-hidden-btn')
+    wraperMobileMenuEl?.classList.remove('no-scroll')
+    wraperMobileMenuEl?.classList.add('is-hidden-mobile')
+    burgerBtnEl?.classList.remove('is-hidden-btn')
+    closeModalBtnEl?.classList.add('is-hidden-btn')
 
 }
