bfdecd696a50df47af60bd0f188767a1a9baf25f
Viktoria Rosovska <torimeister@gmail.com>
2023-06-19 09:32:48 +0300

    work with header

--- src/js/header.js
+++ src/js/header.js
@@ -1,15 +1,15 @@
-const home = document.querySelector('.home-btn');
-const shoplist = document.querySelector('.list-btn');
+const nav = document.querySelector('.header-nav')
+const link = document.querySelectorAll('.js-link');
+// nav.addEventListener('click', (e) => {
+   
+//     console.log(e.target);
+//     console.log(nav)
+//     link.forEach(element => {
+//         element.classList.remove('current');
+//     });
+//     e.target.classList.add('current');
+//     console.log(e.target.href);
+//     window.location.load = e.target.href
+//     e.preventDefault();
+// })
 
-
-shoplist.addEventListener('click', (e) => {
-    e.preventDefault();
-    home.classList.remove('current');
-    shoplist.classList.add('current');
-});
-
-home.addEventListener('click', (e) => {
-    e.preventDefault();
-    shoplist.classList.remove('current');
-    home.classList.add('current');
-});
