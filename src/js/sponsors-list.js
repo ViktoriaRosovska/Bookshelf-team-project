5243ca43db45124a3394508dde052fe9f327383a
Viktoria Rosovska <torimeister@gmail.com>
2023-07-17 23:45:49 +0300

    fix

--- src/js/sponsors-list.js
+++ src/js/sponsors-list.js
@@ -1,18 +1,6 @@
 import { sponsors } from './sponsors-obj';
 import images from '../images/sponsors/*.png';
 
-import save from '../images/sponsors/save_the_children.png';
-import progecthope from '../images/sponsors/progecthope.png';
-import united24 from '../images/sponsors/united24.png';
-import imc from '../images/sponsors/imc.png';
-import medecinssans from '../images/sponsors/medecins_sans.png';
-import razom from '../images/sponsors/razom.png';
-import actionagainst from '../images/sponsors/action_against.png';
-import worldvision from '../images/sponsors/world_vision.png';
-import sergiy_prytula from '../images/sponsors/sergiy_prytula.png';
-
-import Newimage from '../images/sponsors/action_against.png';
-
 const sponsorsList = document.querySelector('.js-sponsors-list');
 let id = 0;
 
@@ -22,53 +10,20 @@ mqLarge?.addEventListener('change', () => (distance = mqLarge.matches ? 3 : 5));
 let distance = mqLarge.matches ? 3 : 5;
 
 function renderSponsorsList() {
-  // const images = [
-  //   save,
-  //   progecthope,
-  //   united24,
-  //   imc,
-  //   medecinssans,
-  //   razom,
-  //   actionagainst,
-  //   worldvision,
-  //   sergiy_prytula,
-  // ];
-
-  // const markup = sponsors
-  //   .map(
-  //     ({
-  //       url,
-  //       title,
-  //       img,
-  //     }) => `<li class="sponsor-item"><span class="sup_number">${pad(
-  //       (id += 1)
-  //     )}</span><a href="${url}" class="sponsor-link" target="_blank"
-  //      noopener noreferrer><img class="sponsor-img" src="${
-  //        images[img]
-  //      }" alt="${title}"/></a></li>`
-  //   )
-  //   .join('');
   const markup = sponsors
     .map(
-      ({
-        url,
-        title,
-        img,
-      }) => `<li class="sponsor-item"><span class="sup_number">${pad(
-        (id += 1)
-      )}</span><a href="${url}" class="sponsor-link" target="_blank"
-       noopener noreferrer><img class="sponsor-img" src="${
-         images[img]
-       }" alt="${title}"/></a></li>`
+      ({ url, title, img }) =>
+        `<li class="sponsor-item"><span class="sup_number">${pad(
+          (id += 1)
+        )}</span><a href="${url}" target="_blank" noopener noreferrer class="sponsor-link1" ><img class="sponsor-img" src="${
+          images[img]
+        }" alt="${title}"/></a></li>`
     )
     .join('');
   sponsorsList?.insertAdjacentHTML('beforeend', markup);
 }
 renderSponsorsList();
 
-{
-  /* <img class="sponsor-img" src="${Newimage}" />; */
-}
 function pad(value) {
   return String(value).padStart(2, '0');
 }
